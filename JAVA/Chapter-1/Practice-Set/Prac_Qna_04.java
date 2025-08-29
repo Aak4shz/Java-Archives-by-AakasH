4a21bce3918c179357733dd3489d0367132759a0
Aakas卄 <akashmunda2005rediffmail.com@gmail.com>
2025-08-29 17:53:43 +0530

    Add Java Question Program

---
+++ JAVA/Chapter-1/Practice-Set/Prac_Qna_04.java
@@ -0,0 +1,18 @@
+// Write a java program the following phase.
+//String s = "123";
+//Convert to int, add 7, print 130.
+//Convert an int back to String and print length.
+
+public class Prac_Qna_04 {
+    public static void main(String[] args) {
+        String zx = "123";
+
+        int num = Integer.parseInt(zx);
+        num = num + 7;
+
+        System.out.println("After Adding 7 : " + num);
+
+        String newStr = Integer.toString(num);
+        System.out.println("Length of \"" + newStr + "\" is " + newStr.length());
+    }
+}
