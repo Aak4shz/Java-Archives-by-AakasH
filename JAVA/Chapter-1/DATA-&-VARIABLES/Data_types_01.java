f94f62dfda8b8a3102ade45c77c7b45ea29716d0
Aakas卄 <akashmunda2005rediffmail.com@gmail.com>
2025-08-29 16:42:40 +0530

    Add few line

--- JAVA/Chapter-1/DATA-&-VARIABLES/Data_types_01.java
+++ JAVA/Chapter-1/DATA-&-VARIABLES/Data_types_01.java
@@ -1,16 +1,16 @@
 // Demonstration of instance and static variables in Java.
 
-public class Data_types_01 {
+public class Data_Types_01 {
 
     public static void main(String[] args) {
 
-        String name = "Rahul"; // instance variable
+        String name = "Sachin ChutMarike"; // instance variable
         int RollNo = 69; // instance variable
-        String SchoolName = "St.Xavier School Bundu."; // static variable
+        String CollegeName = "St.Xavier's College Ranchi."; // static variable
 
         System.out.println("My name is : " + name);
         System.out.println("Roll no is : " + RollNo);
-        System.out.println("And My School is : " + SchoolName);
+        System.out.println("And My College is : " + CollegeName);
 
     }
 }
