e46d9653a271194378b93cef9ca36139a4df61aa
Aakas卄 <akashmunda2005rediffmail.com@gmail.com>
2025-08-29 16:53:32 +0530

    Add java Questions Program

---
+++ JAVA/Chapter-1/Practice-Set/Prac_Qna_01.java
@@ -0,0 +1,11 @@
+// Q1. Write a program to add three numbers and print their sum.
+
+public class Prac_Qna_01 {
+
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20, num3 = 7, sum;
+        sum = num1 + num2 + num3;
+
+        System.out.println("Sum of these numbers : " + sum);
+    }
+}
