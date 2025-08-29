54a90817fbc890a5c96568f6afc284e1d66fea4b
Aakas卄 <akashmunda2005rediffmail.com@gmail.com>
2025-08-29 16:17:59 +0530

    Add Java Programs

---
+++ JAVA/Chapter-1/DATA-&-VARIABLES/Data_Types_02.java
@@ -0,0 +1,12 @@
+public class Data_Types_02 {
+        public static void main(String[] args) {
+        // Calling the Method
+        addNumbers(10, 20);
+    }
+
+    public static void addNumbers(int a, int b) {
+        // Local variable 'total' declared and initialized
+        int total = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is : " + total);
+    }
+}
---
+++ JAVA/Chapter-1/DATA-&-VARIABLES/Data_Types_03.java
@@ -0,0 +1,21 @@
+public class Data_Types_03 {
+    public static void main(String[] args) {
+        byte b = 100;         // 1 byte
+        short s = 10000;      // 2 bytes
+        int i = 100000;       // 4 bytes
+        long l = 1000000000L; // 8 bytes
+        float f = 12.5f;      // 4 bytes (decimal)
+        double d = 123.456;   // 8 bytes (decimal)
+        char c = 'A';         // 2 bytes
+        boolean bool = true;  // 1 bit
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+    }
+}
