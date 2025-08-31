2f0eb802a2cd2bbbb5d69753b80515bc1f94a910
Aakas卄 <akashmunda2005rediffmail.com@gmail.com>
2025-08-31 21:31:32 +0530

    Add java Program

---
+++ JAVA/Chapter-0/Java_Program_0.java
@@ -0,0 +1,16 @@
+// Java Program How to Write code :-
+// Here is Basic Java Program Structure :-
+
+public class Java_Program_0 { 
+// Java is a class-based. EveryThing runs inside a class.
+    public static void main(String[] args) { 
+        // Entry point of every Java program.
+
+        System.out.println("Hello Buddies👋 !"); 
+        // Print commands -->  Statements(code).
+
+        System.out.println("I'm AakasH ");
+        System.out.println("Today I'm starting to learn Java.");
+        
+    }
+}
---
+++ JAVA/Chapter-0/Java_Program_01.java
@@ -0,0 +1,33 @@
+// Simple Java program to demonstrate taking user input and displaying a personalized message.
+
+
+import java.util.Scanner;
+// Give access to Scanner class.
+
+    public class Java_Program_01 {
+        // class declaration
+        public static void main(String[] args) {
+            // Start point of program
+
+        Scanner sc = new Scanner(System.in);
+        // create a Scanner object.
+
+        System.out.println("Enter Your name : " );
+        // Ask the user for input
+
+        String name = sc.nextLine();
+        // Read a line of text
+
+        System.out.println("Enter your age : ");
+        // Ask the user for input
+
+        int age = sc.nextInt();
+        // Read an Integer 
+
+        System.out.println("Hello, " + name + " \nYou are " + age + "years old .");
+        // Display the input back 
+
+        sc.close();
+        // close the Scanner 
+    }
+}
