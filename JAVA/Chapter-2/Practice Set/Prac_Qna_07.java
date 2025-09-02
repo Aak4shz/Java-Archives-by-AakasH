d72041e006601d2e61a8527a21fc30f724f24592
Aakas卄 <akashmunda2005rediffmail.com@gmail.com>
2025-09-02 10:37:05 +0530

    Add Jva Program

--- JAVA/Chapter-2/Practice Set/Prac_Qna_07.java
+++ JAVA/Chapter-2/Practice Set/Prac_Qna_07.java
@@ -9,11 +9,27 @@
             System.out.println("a++ = : " + (a++));
             System.out.println("Now a = : " + a);
 
+            // Post Decrement 
+
+            int x = 11;
+            System.out.println("\nPost Decrement Example : ");
+            System.out.println("x = " + x);
+            System.out.println("x-- = : " + (x--));
+            System.out.println("Now x = : " + x);
+
             // Pre Increment 
             int b = 100;
             System.out.println("\nPre Increment Example : ");
             System.out.println("b = " + b);
             System.out.println("++b = : " + (++b));
             System.out.println("Now b = : " + b);
+
+            // Pre Decrement 
+
+            int z = 111;
+            System.out.println("\nPre Decrement Example : ");
+            System.out.println("z = " + z);
+            System.out.println("--z = : " + (--z));
+            System.out.println("Now z = : " + z);
     }
 }
